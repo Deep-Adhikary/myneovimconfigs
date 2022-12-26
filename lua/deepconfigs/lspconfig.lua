14048edda2199cdc7aeb5081f41ec53039fbab78
Deep Adhikary <adhikary33@gmail.com>
2022-12-26 18:50:39 +0530

    change update_capabilities to default_capabilities

--- lua/deepconfigs/lspconfig.lua
+++ lua/deepconfigs/lspconfig.lua
@@ -39,7 +39,7 @@ local lsp_flags = {
 local nvim_lsp=require('lspconfig')
 local lsp_servers={'pyright','tsserver'}
 for _,lsp in ipairs(lsp_servers) do
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     nvim_lsp[lsp].setup{
         on_attach=on_attach,
         flags=lsp_flags,
